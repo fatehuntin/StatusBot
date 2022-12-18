4bf5c79fa61efc821b13873b8f687a04e438e5da
fatehuntin <nolankh67@gmail.com>
2022-12-18 18:10:15 -0500

    Added example config

---
+++ example_config.py
@@ -0,0 +1,26 @@
+#Read the readme for instructions on how to fill this out
+#Put the uuids you want to track in place of "uuid" in this list
+#multiple uuid's should be in quotes and separated by commas
+#Example: uuid_list = ['069a79f444e94726a5befca90e38aaf5', '8667ba71b85a4004af54457a9734eed7']
+uuid_list = []
+
+
+#The username list should correspond to the order of the uuid's above
+#or else it will display incorrect usernames 
+#Example: username_list ['Notch', 'Steve']
+username_list = []
+
+#API key from hypixel to access their api
+#Example: api_key = 'as1h21huihiuasasbt128741'
+api_key = ''
+#The key to your discord bot
+#Example: KEY = 'ashtqh1uh2412HaHUHATIUSHTASUBTBAS'
+KEY = ''
+#Main channel is the ID of the channel you want most of the messages sent to
+#Example: mainchannel = 12957102731923
+mainchannel = 0
+
+
+#Extra debug stuff, fill in logging channel just in case, it can be the same channel as your main channel
+debug = False
+loggingchannel = 0
--- main.py
+++ main.py
@@ -5,12 +5,7 @@ import asyncio
 import time
 from discord.ext import commands, tasks
 from discord import app_commands
-import pandas as pd
-import plotly.express as px
-from csv import writer
-import datetime
-import numpy as np
-
+from config import uuid_list, username_list, debug, api_key, KEY, mainchannel, loggingchannel
 
 class discoword(discord.Client):
     def __init__(self):
@@ -27,15 +22,12 @@ class discoword(discord.Client):
 
 client = discoword()
 tree = app_commands.CommandTree(client)
-statusNumber = [0,0]
-uuid_list = ['25d9f9ccdfd9491e8f886a48de671510', 'e23703d26f4e43d79d17146821c32943', '05ba7cd0c6f54e198717a919983ed3d6']
-username_list = ['WeeGoJIM', 'FroggyPadi', 'gemstone_nuker']
 online_list = ['False','False','False']
 gamers = []
 @tasks.loop(seconds=5)
 async def status():
     for index, uuid in enumerate(uuid_list):
-        API_data_hypixel = requests.get('https://api.hypixel.net/status?key=4a4bf834-c737-4e2d-89c8-c8506a819e7e&uuid='+uuid)
+        API_data_hypixel = requests.get('https://api.hypixel.net/status?key=' + api_key + '&uuid='+uuid)
         #API_data_mojang = requests.get('https://api.mojang.com/user/profile/' + uuid)
         apidata_hypixel = API_data_hypixel.text
         #apidata_mojang = API_data_mojang.text
@@ -44,13 +36,14 @@ async def status():
         try: 
             online_status = parse_json_apidata_hypixel['session']['online']
         except Exception:
-            await logchannel.send("API error perhaps")
+            if debug:
+                await logchannel.send("API error perhaps")
             pass
         #username = parse_json_apidata_mojang['name']
         username = username_list[index]
         print(online_status, username, online_list[index])
-        channel = client.get_channel(1022187717192323112)
-        logchannel = client.get_channel(996284607404200057)
+        channel = client.get_channel(mainchannel)
+        logchannel = client.get_channel(loggingchannel)
         current_time = int(time.time())
         if online_status:
             statusname = "ONLINE :green_square:"
@@ -76,18 +69,5 @@ async def status():
         elif len(gamers) == 0:
             await client.change_presence(activity=discord.Game(name="No one is online"))
         await asyncio.sleep(5)
-@tree.command(name="graph", description="send graph of cata exp")
-async def self(interaction):
-    df = pd.read_csv("FroggyPadi.csv")
-    fig = px.line(df, x='Time', y='Experience', title='FroggyPadi catacombs experience over time')
-    fig.write_image("graph.png")
-    fig.write_html("graph.html")
-    await interaction.response.send_message(file=discord.File('graph.png'))
-    #await interaction.response.send_message(file=discord.File('graph.html'))
 
-
-
-# only use for main
-client.run('MTA0NjY5MDI4NTI3MjQzNjgxNw.GO6rch.UQURF8AwFVeyW_30gnRSmRmIGrNoRVYCOvlY-Q')
-# only use for testing
-#client.run('MTA0MzIxNzAxNjIyNTUzODExOQ.GG8p2U.6PxFsKzHlYH0sZzSTDfcx4ACGocvkJaFdgOwQQ')
+client.run(KEY)
