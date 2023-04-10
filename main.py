aaa9a6e4fa6540afae32f74f220f0d9f9743637f
fatehuntin <nolankh67@gmail.com>
2023-04-10 11:25:10 -0400

    bools arent subscriptable

--- main.py
+++ main.py
@@ -506,9 +506,9 @@ async def progress():
     newdata = []
 
     for daily_uuidindex, uuid in enumerate(uuid_list):
-        api = skycryptapi_current(uuid_list[daily_uuidindex])[1]
         if not api:
             break
+        api = skycryptapi_current(uuid_list[daily_uuidindex])[1]
         send = False
         embed = discord.Embed(
             title=f"Daily progress update for {username_list[daily_uuidindex]}"
