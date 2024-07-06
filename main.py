32e0c8b22a25f6310475d1dd476bc847799b2f78
fatehuntin <nolankh67@gmail.com>
2024-07-06 13:23:41 -0400

    AUTISMD

--- main.py
+++ main.py
@@ -73,10 +73,10 @@ if not debug:
 # TODO add button under offline msg to view the progress made while the account was online
 @tasks.loop(seconds=10)
 async def status():
+    global statusname, statuscolour, statusemoji, online_time, online_time, sblevel, statusstarted
     if not statusstarted: 
         print("Loading Status...") 
         statusstarted = True
-    global statusname, statuscolour, statusemoji, online_time, online_time, sblevel, statusstarted
     for index, uuid in enumerate(uuid_list):
         parse_json_apidata_hypixel = hypixelapi(uuid, api_key)
         channel = bot.get_channel(mainchannel)
